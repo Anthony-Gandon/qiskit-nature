e670e12c4366d10e20c4560b29fb4518f077e3d7
Anthony-Gandon <antho91370@gmail.com>
2023-05-31 20:45:13 +0200

    Fix init files

--- qiskit_nature/second_q/mappers/__init__.py
+++ qiskit_nature/second_q/mappers/__init__.py
@@ -90,8 +90,7 @@ after the mapping to qubit operators, you can use the following wrapper for symm
 
    TaperedQubitMapper
 
-<<<<<<< HEAD
-
+   
 MixedOp Mappers
 +++++++++++++++
 
@@ -102,8 +101,6 @@ MixedOp Mappers
    MixedMapper
 
 
-=======
->>>>>>> main
 Qubit Converter
 +++++++++++++++
 
@@ -127,10 +124,7 @@ from .qubit_mapper import QubitMapper
 from .qubit_converter import QubitConverter
 from .interleaved_qubit_mapper import InterleavedQubitMapper
 from .tapered_qubit_mapper import TaperedQubitMapper
-<<<<<<< HEAD
 from .mixed_mapper import MixedMapper
-=======
->>>>>>> main
 
 __all__ = [
     "BravyiKitaevMapper",
@@ -141,12 +135,9 @@ __all__ = [
     "LinearMapper",
     "BosonicLinearMapper",
     "LogarithmicMapper",
+    "MixedMapper",
     "QubitConverter",
     "QubitMapper",
     "InterleavedQubitMapper",
     "TaperedQubitMapper",
-<<<<<<< HEAD
-    "MixedMapper",
-=======
->>>>>>> main
 ]
